656ea4c93936da3f6b865262e56a80bf5bc92021
Nirav Patel <nd_ce06@yahoo.co.in>
2019-08-26 13:24:49 -0400

    corrected size component

--- src/app/settings/size/size.component.ts
+++ src/app/settings/size/size.component.ts
@@ -1,38 +1,58 @@
-<ActionBar class="action-bar">
-    <!-- 
-    Use the NavigationButton as a side-drawer button in Android
-    because ActionItems are shown on the right side of the ActionBar
-    -->
-    <NavigationButton ios:visibility="collapsed" icon="res://menu" (tap)="onDrawerButtonTap()"></NavigationButton>
-    <!-- 
-    Use the ActionItem for IOS with position set to left. Using the
-    NavigationButton as a side-drawer button in iOS is not possible,
-    because its function is to always navigate back in the application.
-    -->
-    <ActionItem icon="res://navigation/menu" android:visibility="collapsed" (tap)="onDrawerButtonTap()"
-        ios.position="left">
-    </ActionItem>
-    <Label class="action-bar-title" text="Size Settings"></Label>
-</ActionBar>
-<StackLayout sdkExampleTitle sdkToggleNavButton>
-    <GridLayout rows="auto *">
-        <SearchBar row="0" [hint]="searchBarHint" (clear)="onClear($event)" (textChange)="onTextChange($event)">
-        </SearchBar>
+import { Component, OnInit } from "@angular/core";
+import { NavigationExtras } from "@angular/router";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
+import { RouterExtensions, } from "nativescript-angular/router";
+import { SearchBar } from "tns-core-modules/ui/search-bar";
+import * as app from "tns-core-modules/application";
+import { SizeService } from "../../services";
+import { ISize } from "../../interface";
 
-        <ListView row="1" [items]="sizeList | filter:searchText:'name'" class="list-group">
-            <ng-template let-item="item" let-i="index" let-odd="odd" let-even="even">
-                <StackLayout orientation="horizontal" class="list-group-item" [class.odd]="odd" [class.even]="even">
-                    <Label [text]="item.name" textWrap="true"></Label>
-                    <Label text=" ({{item.unit}})"></Label>
-                    <Button class="btn" pageTransition="slide" text="&#xf044;" (tap)="editItem(item)"></Button>
-                </StackLayout>
-            </ng-template>
-        </ListView>
+@Component({
+    selector: "Size",
+    moduleId: module.id,
+    templateUrl: "./size.component.html",
+    providers: [SizeService]
+})
+export class SizeComponent implements OnInit {
+    private sizeList: Array<ISize>;
+    private searchText: string;
+    private searchBarHint: string = 'Search...';
+    constructor(private sizeService: SizeService, private routerExtension: RouterExtensions) {
+        // Use the component constructor to inject providers.
+    }
+
+    ngOnInit(): void {
+        // Init your component properties here.
+
+        this.sizeService.getSizeList()
+            .then((res) => {
+                this.sizeList = res;
+            });
+    }
 
 
-        <AbsoluteLayout verticalAlignment="bottom" horizontalAlignment="right" row="1">
-            <Button class="btn btn-primary m-5 fa btn-rounded-lg" [nsRouterLink]="['/settings/size/edit']"
-                pageTransition="slide" text="&#xf067;"></Button>
-        </AbsoluteLayout>
-    </GridLayout>
-</StackLayout>
+    public onTextChange(args) {
+        let searchBar = <SearchBar>args.object;
+        this.searchText = searchBar.text.toLowerCase();
+
+    }
+
+    public onClear(args) {
+        let searchBar = <SearchBar>args.object;
+        searchBar.text = "";
+        searchBar.hint = this.searchBarHint;
+        this.searchText = "";
+    }
+
+    public editItem(item: ISize) {
+        const navigationExtras: NavigationExtras = {
+            queryParams: { ...item }
+        }
+        this.routerExtension.navigate(['/settings/size/edit'], navigationExtras);
+    }
+
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
+    }
+}
